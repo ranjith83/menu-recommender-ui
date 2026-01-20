641df7c5c4d6db7e63f5466f30c15bdce3f411cd
Ranjith <ranji_b4u@yahoo.co.in>
2026-01-20 10:44:53 +0000

    change the api url

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { MenuItem } from '../models/menu-item.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
-  private apiUrl = 'http://localhost:7084/api/menu-items';
+  private apiUrl = `${environment.apiUrl}/menu`;
 
   constructor(private http: HttpClient) {}
 
