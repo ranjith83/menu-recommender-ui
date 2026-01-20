1c3c90fb0bb3d6b758158d5122175a9d3304d8f7
Ranjith <ranji_b4u@yahoo.co.in>
2026-01-20 10:06:33 +0000

    fix for prod api routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,27 @@
+
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'menu',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'basket',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'order-status/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'kitchen-login',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'kitchen',
+    renderMode: RenderMode.Client
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
